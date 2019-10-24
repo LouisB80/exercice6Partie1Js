d615c4215ce8278b5d1b48bf2bd62ecf583f4916
LouisB80 <louisbernardh@gmail.com>
2019-10-24 09:37:18 +0200

    first commit

---
+++ assets/js/exercice6.js
@@ -0,0 +1,8 @@
+function result()
+{
+  var number1, number2, result;
+  number1 = document.getElementById('firstNumber').value;
+  number2 = document.getElementById('secondNumber').value;
+  result = number1 % number2;
+  alert(result);
+}
